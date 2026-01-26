da48e337c001fc3b98c5ba544cfd213796e0e0ae
varshini-student <mvarshini1708@gmail.com>
2026-01-26 17:20:25 +0530

    Added remaining C programs

--- If Conditions/simple_calculator.c
+++ If Conditions/simple_calculator.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  scanf("%d%d",&a,&b);
+  int c;
+  scanf("%d",&c);
+  switch(c){
+      case 1:
+      printf("%d",a+b);
+      break;
+      printf("Invalid");
+  }
+    return 0;
+}
 
