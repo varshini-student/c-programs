5ed194b273278fb0764f145d821051ee7050a7f8
varshini-student <mvarshini1708@gmail.com>
2026-01-28 16:13:39 +0530

    Addded  count digits

---
+++ Looping Statements/count_digits.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    int count=0;
+    for(;a!=0;a=a/10) {
+        int num=a%10;
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+}
