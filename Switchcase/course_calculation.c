54bf91c8e0deb9f17c5c98e63aa70f82542e2543
varshini-student <mvarshini1708@gmail.com>
2026-01-28 08:00:44 +0530

    Added reamaining c problems in switch case

---
+++ Switchcase/course_calculation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int mode;
+    char category;
+    int fee = 0;
+    scanf("%d", &mode);
+    scanf(" %c", &category);
+    switch(mode) {
+        case 1: 
+            switch(category) {
+                case 'R':
+                    fee = 5000;
+                    break;
+                case 'S':
+                    fee = 3000;
+                    break;
+            }
+            break;
+        case 2: 
+            switch(category) {
+                case 'R':
+                    fee = 9000;
+                    break;
+                case 'S':
+                    fee = 7000;
+                    break;
+            }
+            break;
+        default:
+            fee = 0; 
+    }
+    printf("₹%d", fee);
+    return 0;
+}
