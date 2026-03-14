18503a46eab455f81a3de157dd00e2e088822922
varshini-student <mvarshini1708@gmail.com>
2026-03-14 15:51:37 +0530

    added even numbers

---
+++ 1D Array/even.numbers.c
@@ -0,0 +1,22 @@
+array using even numbers
+
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   int arr[50];
+   int count; 
+      scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0)
+        count++;
+    }
+    printf("%d",count);
+
+    return 0;
+}
