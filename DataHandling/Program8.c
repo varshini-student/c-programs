f53623e5eeea6eef00c954b9efed1c8c1090fd01
varshini-student <mvarshini1708@gmail.com>
2026-05-27 10:33:55 +0530

    print the area of a circle

--- DataHandling/Program8.c
+++ DataHandling/Program8.c
@@ -1,4 +1,4 @@
-// print the total sum of thenumbers
+// print the fahrenheit value
 #include<stdio.h>
 int main(){
     float c,f;
---
+++ DataHandling/Program9.c
@@ -0,0 +1,8 @@
+// print the  area of the circle
+#include<stdio.h>
+int main(){
+    float r,a;
+    scanf("%f",&r);
+    f=(3.14159)*r*r;
+    printf("%3.f",a);
+}
